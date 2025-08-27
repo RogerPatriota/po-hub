493552c73fa6811689f08cfd0587809d5676dc7d
roger <rogerpatriota@gmail.com>
2025-08-27 07:22:38 -0300

    core: add db connection logs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ProjectModule } from './projects/project.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
   imports: [
@@ -9,7 +10,16 @@ import { MongooseModule } from '@nestjs/mongoose';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
       process.env.MONGO_URL_CONNECTION ?? '',
-      { dbName: process.env.DATABASE_NAME ?? '' }
+      { dbName: process.env.DATABASE_NAME ?? '',
+        onConnectionCreate: (connection: Connection) => {
+          connection.on('connected', () => console.log('Mongo connected'))
+          connection.on('open', () => console.log('Mongo on'))
+          connection.on('disconnected', () => console.log('Mongo disconnected'));
+          connection.on('reconnected', () => console.log('Mongo reconnected'));
+
+          return connection
+        }
+       },
     ),
   ],
 })
