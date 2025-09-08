84bfa30262963edf8822514f100a83fc38fb94f4
roger <rogerpatriota@gmail.com>
2025-09-07 21:49:34 -0300

    feat: add project repository

--- backend/src/projects/project.controller.ts
+++ backend/src/projects/project.controller.ts
@@ -1,9 +1,14 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { CreateProjectDTO } from "./dto/create.project.dto";
+import { ProjectRepository } from "./project.repository";
 
 
 @Controller('project')
 export class ProjectController {
+
+    constructor(
+        private projectRepository: ProjectRepository
+    ) {}
 
     @Get()
     async check() {
@@ -12,7 +17,12 @@ export class ProjectController {
 
     @Post()
     async createProject(@Body() projectData: CreateProjectDTO) {
+
+        const newProject = this.projectRepository.createProject(projectData)
+
+        if (newProject) {
+            return { data: projectData }
+        }
         
-        return { data: projectData }
     }
 }
--- backend/src/projects/project.repository.ts
+++ backend/src/projects/project.repository.ts
@@ -1,3 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Project } from "./schemas/project.schema";
+import { Model } from "mongoose";
+import { CreateProjectDTO } from "./dto/create.project.dto";
 
 
-export class ProjectRepository {}
+@Injectable()
+export class ProjectRepository {
+
+    constructor(
+        @InjectModel(Project.name) private projectModel: Model<Project>
+    ) {}
+
+    async createProject(project: CreateProjectDTO) {
+        const newProject = new this.projectModel(project)
+        
+        return newProject
+    }
+}
